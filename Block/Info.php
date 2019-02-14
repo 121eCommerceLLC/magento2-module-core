bdc95e92cf33c5c92ac16b7d00d6a7a414f2a537
Gera <cala932@gmail.com>
2019-02-14 16:49:19 -0300

    Update 121 Logo

--- Block/Info.php
+++ Block/Info.php
@@ -4,7 +4,8 @@ namespace Ecommerce121\Core\Block;
 use Magento\Framework\Data\Form\Element\AbstractElement;
 use Magento\Framework\Data\Form\Element\Renderer\RendererInterface;
 use Magento\Backend\Block\AbstractBlock;
-
+use Magento\Backend\Block\Context;
+use Magento\Framework\View\Asset\Repository;
 /**
  * Class Info
  * @package Ecommerce121\Core\Block
@@ -12,13 +13,36 @@ use Magento\Backend\Block\AbstractBlock;
 class Info extends AbstractBlock implements RendererInterface
 {
     /**
+     * @var Repository
+     */
+    protected $repository;
+
+    /**
+     * Info constructor.
+     * @param Context $context
+     * @param Repository $repository
+     * @param array $data
+     */
+    public function __construct(
+        Context $context, Repository $repository, array $data = [])
+    {
+        $this->repository = $repository;
+        parent::__construct($context, $data);
+    }
+
+    /**
      * @param AbstractElement $element
      * @return string
      */
     public function render(AbstractElement $element)
     {
         $element = null;
-        $logo = 'https://www.121ecommerce.com/hubfs/2017%20Redesign/logo.png';
+        $fileId = 'Ecommerce121_Core::images/logo121.svg';
+        $params = [
+            'area' => 'adminhtml'
+        ];
+        $asset = $this->repository->createAsset($fileId, $params);
+        $logo = $asset->getUrl();
         $html = '<div style="background: url('.$logo.')no-repeat scroll 15px 15px #f8f8f8; 
                 border:1px solid #ccc; min-height:100px; margin:5px 0; 
                 padding:15px 15px 15px 140px;">
